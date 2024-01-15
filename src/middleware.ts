a7a4ed56e049fcf2669fe6d3d0ec320fd24f5078
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-01-15 17:00:17 +0100

    fix: always regex

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,5 +8,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)", '/s/*'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)", '/s/(.*)'],
 };
